1a1c81c7cd578928f19c49d2cc578197ecb4f8b0
iain26 <icotte200@caledonian.ac.uk>
2018-03-19 14:29:37 +0000

    Charge

---
+++ Assets/Scripts/ChargeScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChargeScript : MonoBehaviour {
+
+    public GameObject EndPanel;
+    public Text scoreText;
+
+    private int score;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        print("Col");
+        if(collision.collider.tag == "Royalist")
+        {
+            EndPanel.SetActive(true);
+            Time.timeScale = 0;
+            scoreText.text = "Score: " + score.ToString();
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        float increment = Time.deltaTime;
+        Vector3 lastPos = transform.localPosition;
+        transform.localPosition = new Vector3(lastPos.x - increment, lastPos.y, lastPos.z);
+    }
+}
---
+++ Assets/Scripts/FireScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireScript : MonoBehaviour {
+
+    bool reloading = false;
+
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void Fire()
+    {
+        print(gameObject.name + " can Fire");
+    }
+
+    IEnumerator Reload()
+    {
+        yield return new WaitForSeconds(4);
+        reloading = false;
+        yield return 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0) || Input.touchCount > 0)
+        {
+            RaycastHit hit;
+            Ray ray;
+            // for debugging on PC
+            if (Application.platform == RuntimePlatform.Android)
+            {
+                ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            }
+            else
+            {
+                ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            }
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.gameObject == this.gameObject)
+                {
+                    if (!reloading)
+                    {
+                        reloading = true;
+                        StartCoroutine(Reload());
+                        Fire();
+                    }
+                }
+            }
+        }
+    }
+}
